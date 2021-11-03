1ef382923e01dc2f09f9b01a73c27854c72cb58c
Youhei Oonuma <oonuma617@gmail.com>
2021-11-03 16:23:38 +0900

    no use wildcard import

--- app/src/main/java/com/example/composemvvm/MainActivity.kt
+++ app/src/main/java/com/example/composemvvm/MainActivity.kt
@@ -6,7 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
