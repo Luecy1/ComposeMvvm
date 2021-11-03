afb3c6f96ba9c6da3063b9c9cdf6b989ab3a3007
Youhei Oonuma <oonuma617@gmail.com>
2021-11-03 16:17:02 +0900

    clean

--- app/src/main/java/com/example/composemvvm/MainActivity.kt
+++ app/src/main/java/com/example/composemvvm/MainActivity.kt
@@ -5,23 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-
-import com.example.composemvvm.ui.theme.ComposeMvvmTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeMvvmTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    CountUp()
-                }
-            }
+            CountUp()
         }
     }
 }
