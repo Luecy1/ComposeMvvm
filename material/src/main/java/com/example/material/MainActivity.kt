4d456c64faed9e2b5cf2097acf76022e86a17353
Luecy1 <oonuma617@gmail.com>
2021-11-23 00:06:13 +0900

    List化

--- material/src/main/java/com/example/material/MainActivity.kt
+++ material/src/main/java/com/example/material/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.material
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -18,12 +26,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.material.ui.theme.ComposeMvvmTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -31,23 +41,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
-            Navigation(navController = navController)
+            MyNavigation(navController = navController)
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeMvvmTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    ComposeMvvmTheme {
+//        Greeting("Android")
+//    }
+//}
 
 @Composable
 fun BottomBar(selectedItem: MutableState<Int>) {
@@ -88,7 +98,7 @@ fun BottomBar() {
 }
 
 @Composable
-fun Navigation(navController: NavHostController) {
+fun MyNavigation(navController: NavHostController) {
     NavHost(navController = navController, startDestination = "profile") {
         composable("profile") {
             Button(onClick = {
@@ -102,4 +112,38 @@ fun Navigation(navController: NavHostController) {
             Text("friendslist")
         }
     }
+}
+
+@Composable
+fun PageListScreen() {
+    Column(
+        Modifier.scrollable(
+            orientation = Orientation.Horizontal,
+            state = rememberScrollState(),
+        ),
+    ) {
+
+        repeat(20) {
+            Text(
+                text = "BottomBar",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                    }
+                    .padding(12.dp),
+                fontSize = 24.sp,
+            )
+            Divider()
+        }
+        Text(
+            text = "Go to Profile",
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PageListScreenPreview() {
+    PageListScreen()
 }
